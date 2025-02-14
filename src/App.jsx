e56c7d3a0902163249d4b014595d49ad9e5c6b78
aayushsoodhp@gmail.com <aayushsoodhp@gmail.com>
2025-02-14 21:50:23 +0530

    Fetch function implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,36 @@
+import { useState, useEffect } from "react";
+
 function App() {
+
+  const [toDoList , setToDoList] = useState([]);
+  const [loading , setLoading] = useState(false);
+  const [errorMsg , setErrorMsg] = useState('');
+  async function fetchToDoList(){
+    try{
+      setLoading(true);
+      const apiResponse = await fetch('https://dummyjson.com/todos');
+      const response = await apiResponse.json();
+
+      if(response?.todos && response?.todos?.length > 0){
+        setToDoList(response?.todos);
+        setLoading(false);
+        setErrorMsg('');
+      }
+      else{
+        setToDoList([]);
+        setLoading(false);
+        setErrorMsg('');
+      }
+    } catch(err){
+      setToDoList([]);
+      setErrorMsg('Error has occured');
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchToDoList();
+  } , [])
 
   return (
     <div>
